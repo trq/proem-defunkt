025363e2f87a2bb9c0161600db64d7f81b40655d
Tony R Quilkey <trq@thorpesystems.com>
2011-01-12 14:26:14 +1100

    Improved the _Command_ class to handle more (dynamic) properties.

--- lib/Proem/Controller/Route/Command.php
+++ lib/Proem/Controller/Route/Command.php
@@ -18,16 +18,25 @@ namespace Proem\Controller\Route;
  */
 class Command
 {
-    public $controller = null;
-    public $action = null;
-    public $params = array();
+    private $_data = array();
+
+    public function __set($name, $value) {
+        $this->_data[$name] = $value;
+    }
+
+    public function __get($name) {
+        if (array_key_exists($name, $this->_data)) {
+            return $this->_data[$name];
+        }
+        return $this;
+    }
 
     public function parseParams()
     {
         $tmp = array();
-        for ($i = 0; $i <= count($this->params); $i = $i+2) {
-            if (isset($this->params[$i+1])) {
-                $tmp[$this->params[$i]] = $this->params[$i+1];
+        for ($i = 0; $i <= count($this->_data['params']); $i = $i+2) {
+            if (isset($this->_data['params'][$i+1])) {
+                $tmp[$this->_data['params'][$i]] = $this->_data['params'][$i+1];
             } else {
                 break;
             }
